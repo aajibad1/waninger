787ee6e0d443ba5ad8dfe099520a940642b2b8b6
lukew <luke.waninger@gmail.com>
2018-06-26 00:52:53 -0700

    '.'

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ from flask import (
     Flask, Blueprint, render_template
 )
 
-BP = Blueprint('default', __name__, template_folder='templates')
+BP = Blueprint('default', __name__, template_folder='waninger/templates')
 
 
 @BP.route('/')
