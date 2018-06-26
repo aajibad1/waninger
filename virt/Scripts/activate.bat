517299a30af9ca01a4a3305c0d6a0a695e920b0d
lukew <luke.waninger@gmail.com>
2018-06-26 10:37:29 -0700

    update environmnet

--- virt/Scripts/activate.bat
+++ virt/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\lukew\OneDrive\waninger\virt"
+set "VIRTUAL_ENV=C:\Users\lukew\OneDrive\git\waninger\virt"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
