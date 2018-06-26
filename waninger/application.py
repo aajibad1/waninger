fb2fed688bfbab2b6d6de6f93144d8379f11815b
lukew <luke.waninger@gmail.com>
2018-06-26 00:28:29 -0700

    '.'

--- waninger/application.py
+++ waninger/application.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Flask Application Factory
+
+This module implements the flask application, blueprints, and routes.
+"""
+
 from flask import (
     Flask, Blueprint, render_template
 )
@@ -7,25 +13,30 @@ bp = Blueprint('default', __name__, template_folder='templates')
 
 @bp.route('/')
 def home():
+    """register route to home"""
     return render_template('index.html')
 
 
 @bp.route('/projects')
 def projects():
+    """register route to projects"""
     return render_template('projects.html')
 
 
 @bp.route('/resume')
 def resume():
+    """register route to resume"""
     return render_template('resume.html')
 
 
 @bp.route('/contact')
 def contact():
+    """register route to contact form"""
     return render_template('contact.html')
 
 
 def create_app(test_config=None):
+    """generate the application"""
     app = Flask(__name__, instance_relative_config=True)
 
     # load the app config
