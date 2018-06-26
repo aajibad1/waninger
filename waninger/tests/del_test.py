18a1f19daf609ec590ae828acdeafeb866f0c381
lukew <luke.waninger@gmail.com>
2018-06-26 00:40:47 -0700

    '.'

--- waninger/tests/del_test.py
+++ waninger/tests/del_test.py
@@ -1,6 +1,14 @@
+# -*- coding: utf-8 -*-
+"""delete the test file
+
+delete this
+"""
+
 from unittest import TestCase
 
 
 class TestRepository(TestCase):
+    """a class to delete"""
     def test_nothing(self):
-        self.assertTrue(True)
+        """a unit test to delete"""
+        self.assertEquals(1, 1)
