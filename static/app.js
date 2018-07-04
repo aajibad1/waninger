c21f0e098659dcf4467f7409938eac998c978f75
lukew <luke.waninger@gmail.com>
2018-07-03 23:37:57 -0700

    .

--- static/app.js
+++ static/app.js
@@ -1,22 +1,7 @@
 function get_random_featured() {
-    $.ajax({
-        'type': 'GET',
-        'url': '/get_random_featured',
-        'async': false,
-        'cache': true,
-        'success': function(data) {
-            if (CLASS_TOGGLE) {
-                CLASS_TOGGLE ^= true
-                $('#featured_img').removeClass("");('src', data)
-        },
-        'failed': function() {
-            console.log('featured image failed to udpate')
-        }
-    })
+    location.reload(true)
 }
 
 $(document).ready(function() {
-    CLASS_TOGGLE = true;
-
-    setTimeout(get_random_featured, 5000)
+    setTimeout(get_random_featured, 30000)
 });
